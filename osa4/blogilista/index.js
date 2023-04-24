e11bc3129b4503660a20e14e04bc1e2ea65bf34e
Helmi Ikonen <helmi.ikonen@tuni.fi>
2023-04-24 10:44:56 +0300

    tehtävät 4.8.-4.12

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,16 +2,6 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-// const Blog = mongoose.model('Blog', blogSchema)
-// const mongoUrl = process.env.MONGODB_URI
-// mongoose.connect(mongoUrl)
-
-// app.use(cors())
-// app.use(express.json())
-
-
-
-
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
---
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -0,0 +1,147 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+describe('api tests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+
+  test('there is a field named id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
+
+  test('blogs can be added to the database', async () => {
+    const newBlog = {
+      title: 'Testiblogi',
+      author: 'Testi Testinen',
+      url: 'testiurl.com',
+      likes: 2
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+    
+    const blogsResponse = await Blog.find({})
+    const blogsAtEnd = blogsResponse.map(blog => blog.toJSON())
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+    const blogs = blogsAtEnd.map(blog => blog.title)
+    expect(blogs).toContain('Testiblogi')
+  })
+
+  test('blog with empty likes field gets 0 likes', async () => {
+    const newBlog = {
+      title: 'Blogitesti',
+      author: 'Tero Testaaja',
+      url: 'blogitesti.com'
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+    
+    const blogWithZeroLikes = await Blog.find({ title: 'Blogitesti' })
+    const blogJSON = blogWithZeroLikes[0].toJSON()
+    expect(blogJSON.likes).toBe(0)
+  })
+
+  test('missing title and/or url field cause status code 400', async () => {
+    const blogWithNoTitle = {
+      author: 'Joku Testihenkilö',
+      url: 'testihenkilon-blogi.com'
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(blogWithNoTitle)
+    .expect(400)
+
+    const blogWithNoUrl = {
+      author: 'Joku Testihenkilö',
+      title: 'Otsikko'
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(blogWithNoUrl)
+    .expect(400)
+
+  })
+
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
